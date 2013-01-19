ce7fc3293bb590198d13f6d197606886113666cc
Nathan <n.gaberel@gmail.com>
2013-01-19 17:36:30 +0100

    Nettoyage du code.

--- src/Ardrone/ARDrone.java
+++ src/Ardrone/ARDrone.java
@@ -1,50 +1,4 @@
 package Ardrone;
-/*
-Author: MAPGPS on https://projects.ardrone.org
-Initial: 2010.09.20
-Updated: 2010.09.24
-
-UI_BIT:
-00010001010101000000000000000000
-   |   | | | |        || | ||||+--0: Button turn to left
-   |   | | | |        || | |||+---1: Button altitude down (ah - ab)
-   |   | | | |        || | ||+----2: Button turn to right
-   |   | | | |        || | |+-----3: Button altitude up (ah - ab)
-   |   | | | |        || | +------4: Button - z-axis (r1 - l1)
-   |   | | | |        || +--------6: Button + z-axis (r1 - l1)
-   |   | | | |        |+----------8: Button emergency reset all
-   |   | | | |        +-----------9: Button Takeoff / Landing
-   |   | | | +-------------------18: y-axis trim +1 (Trim increase at +/- 1??/s)
-   |   | | +---------------------20: x-axis trim +1 (Trim increase at +/- 1??/s)
-   |   | +-----------------------22: z-axis trim +1 (Trim increase at +/- 1??/s)
-   |   +-------------------------24: x-axis +1
-   +-----------------------------28: y-axis +1
-
-AT*REF=<sequence>,<UI>
-AT*PCMD=<sequence>,<enable>,<pitch>,<roll>,<gaz>,<yaw>
-	(float)0.01 = (int)1008981770		(float)-0.01 = (int)-1138501878
-	(float)0.05 = (int)1028443341		(float)-0.05 = (int)-1119040307
-	(float)0.1  = (int)1036831949		(float)-0.1  = (int)-1110651699
-	(float)0.2  = (int)1045220557		(float)-0.2  = (int)-1102263091
-	(float)0.5  = (int)1056964608		(float)-0.5  = (int)-1090519040
-AT*ANIM=<sequence>,<animation>,<duration>
-AT*CONFIG=<sequence>,\"<name>\",\"<value>\"
-
-altitude max2m:	java ARDrone 192.168.1.1 AT*CONFIG=1,\"control:altitude_max\",\"2000\"
-Takeoff:	java ARDrone 192.168.1.1 AT*REF=101,290718208
-Landing:	java ARDrone 192.168.1.1 AT*REF=102,290717696
-Hovering:	java ARDrone 192.168.1.1 AT*PCMD=201,1,0,0,0,0
-gaz 0.1:	java ARDrone 192.168.1.1 AT*PCMD=301,1,0,0,1036831949,0
-gaz -0.1:	java ARDrone 192.168.1.1 AT*PCMD=302,1,0,0,-1110651699,0
-pitch 0.1:	java ARDrone 192.168.1.1 AT*PCMD=303,1,1036831949,0,0,0
-pitch -0.1:	java ARDrone 192.168.1.1 AT*PCMD=304,1,-1110651699,0,0,0
-yaw 0.1:	java ARDrone 192.168.1.1 AT*PCMD=305,1,0,0,0,1036831949
-yaw -0.1:	java ARDrone 192.168.1.1 AT*PCMD=306,1,0,0,0,-1110651699
-roll 0.1:	java ARDrone 192.168.1.1 AT*PCMD=307,1,0,1036831949,0,0
-roll -0.1:	java ARDrone 192.168.1.1 AT*PCMD=308,1,0,-1110651699,0,0
-pitch -30 deg:	java ARDrone 192.168.1.1 AT*ANIM=401,0,1000
-pitch 30 deg:	java ARDrone 192.168.1.1 AT*ANIM=402,1,1000
-*/
 
 import java.net.*;
 import java.util.*;
@@ -64,12 +18,11 @@ public class ARDrone {
 	public ARDrone (VideoPanel vpanel) throws Exception
 	{
 		this.vpanel=vpanel;
-		System.out.println("Lancement ARDrone ...");
 	}
 	
 	public void init () throws Exception
 	{
-		String cmd;
+		System.out.println("Initialisation ARDrone ...");
 		
 		// On réécrit l'ip dans un objet InetAddress
 		StringTokenizer st = new StringTokenizer(ip, ".");
@@ -88,54 +41,37 @@ public class ARDrone {
 		socketVideo = new Socket(inet_addr, 5555);
 		socketVideo.setSoTimeout(3000);
 
-		// Initialisation de la rï¿½ception des navdata (cf p.41 du pdf)
-		byte[] buffer = {01, 00, 00, 00};
-		DatagramPacket tickle = new DatagramPacket(buffer, buffer.length, inet_addr, 5554);
+		// Initialisation de la réception des navdata (cf p.41 du pdf)
+		byte[] bufferTickle = {01, 00, 00, 00};
+		DatagramPacket tickle = new DatagramPacket(bufferTickle, bufferTickle.length, inet_addr, 5554);
 
-		cmd = "AT*CONFIG="+seq+",\"general:navdata_demo\",\"TRUE\"";
-		seq++;
-		send(cmd);
+		send("AT*CONFIG="+(seq++)+",\"general:navdata_demo\",\"TRUE\"");
 		
-		cmd = "AT*FTRIM="+(seq++);
-		send(cmd);
+		send("AT*FTRIM="+(seq++));
 		
 		socketNavdata.send(tickle);
 		
-		cmd = "AT*PMODE="+(seq++)+",2";
-		send(cmd);
+		send("AT*PMODE="+(seq++)+",2");
 		
-		cmd = "AT*MISC="+(seq++)+",20,2000,3000";
-		send(cmd);
+		send("AT*MISC="+(seq++)+",20,2000,3000");
 		
-		cmd = "AT*FTRIM="+(seq++);
-		send(cmd);
-		
-		/*cmd = "AT*CTRL="+seq+",5,0";
-		seq++;
-		send(cmd);*/
+		send("AT*FTRIM="+(seq++));
 
-		// Init du thread watchdog + rï¿½cup navdata
+		// Init du thread watchdog + récup navdata
 		ThreadNavdata Tn = new ThreadNavdata("TNavdata", this);
 		Tn.start();
 		
-		socketVideo.getOutputStream().write(buffer);
+		socketVideo.getOutputStream().write(bufferTickle);
 		
-		cmd = "AT*CONFIG="+(seq++)+",\"general:video_enable\",\"TRUE\"";
-		send(cmd);
+		send("AT*CONFIG="+(seq++)+",\"general:video_enable\",\"TRUE\"");
 		
-		socketVideo.getOutputStream().write(buffer);
+		socketVideo.getOutputStream().write(bufferTickle);
 		
-		cmd = "AT*CONFIG="+(seq++)+",\"video:bitrate_ctrl_mode\",\"0\"";
-		send(cmd);
+		send("AT*CONFIG="+(seq++)+",\"video:bitrate_ctrl_mode\",\"0\"");
 		
 		ThreadVideo Tv = new ThreadVideo(socketVideo, vpanel);
 		Tv.start();
 		
-		// Horizontal trim
-		//cmd = "AT*FTRIM="+(seq++);
-		//send(cmd);
-		
-		//Runtime.getRuntime().exec("C:/ffmpeg/bin/ffplay tcp://192.168.1.1:5555");
 		System.out.println("\tARDrone pret.");
 	}
 	
@@ -151,8 +87,7 @@ public class ARDrone {
 	{
 		while (!this.navdata.emergency())
 		{
-			String str = "AT*REF=" + (seq++) + ",290717952";
-			send(str);
+			send("AT*REF=" + (seq++) + ",290717952");
 			Thread.sleep(5);
 		}
 		System.out.println("\t["+seq+"] Atterissage d'urgence reussi !");
@@ -162,8 +97,7 @@ public class ARDrone {
 	{
 		while (!this.navdata.flying())
 		{
-			String str = "AT*REF=" + (seq++) + ",290718208";
-			send(str);
+			send("AT*REF=" + (seq++) + ",290718208");
 			Thread.sleep(5);
 		}
 		System.out.println("\t["+seq+"] Decollage réussi !");
@@ -173,14 +107,13 @@ public class ARDrone {
 	{
 		while (this.navdata.flying())
 		{
-			String str = "AT*REF=" + (seq++) + ",290717696";
-			send(str);
+			send("AT*REF=" + (seq++) + ",290717696");
 			Thread.sleep(5);
 		}
-		System.out.println("\t["+seq+"] Atterrissage russi !");
+		System.out.println("\t["+seq+"] Atterrissage réussi !");
 	}
 
-	/* Dï¿½placement : angles phi (horizontal latï¿½ral), theta (horizontal avant/arriï¿½re) et vitesses z (verticale), psi (angulaire verticale)
+	/* Déplacement : angles phi (horizontal latéral), theta (horizontal avant/arrière) et vitesses z (verticale), psi (angulaire verticale)
 	 * Les arguments sont des pourcentages de l'angle (la vitesse) maximal ([-1 ; 1]) */
 	public void deplacement(float phi, float theta, float vspeed, float aspeed) throws Exception
 	{
@@ -189,15 +122,12 @@ public class ARDrone {
 		int theta2 = (theta>-0.1 && theta<0.1) ? 0 : Float.floatToIntBits(theta);
 		int vspeed2 = (vspeed>-0.1 && vspeed<0.1) ? 0 : Float.floatToIntBits(vspeed);
 		int aspeed2 = (aspeed>-0.1 && aspeed<0.1) ? 0 : Float.floatToIntBits(aspeed);
-		String str = "AT*PCMD=" + seq + "," + (CombinedYaw ? 3 : 1) + ","+phi2+","+theta2+","+vspeed2+","+aspeed2;
-		seq++;
-		send(str);
+		send("AT*PCMD=" + (seq++) + "," + (CombinedYaw ? 3 : 1) + ","+phi2+","+theta2+","+vspeed2+","+aspeed2);
 	}
 	
 	public void stationnaire() throws Exception
 	{
-		String s = "AT*PCMD="+ (seq++) +",1,0,0,0,0";
-		send(s);
+		send("AT*PCMD="+ (seq++) +",1,0,0,0,0");
 	}
 	
 	public boolean enVol ()
